c26c3f179b68ed20a1af78715faa632d10e5268e
toddyhan <905212484@qq.com>
2017-09-08 22:11:31 +0800

    functions

--- TestPycharm/TestFuntion.py
+++ TestPycharm/TestFuntion.py
@@ -37,3 +37,34 @@ y_copy = [1,2]
 changer_copy(x_copy,y_copy)
 print ("x_copy: ", x_copy)
 print ("y_copy:", y_copy)
+
+def ref_value(x, y, name):
+    print ("x: ", x)
+    print ("y: ", y)
+    print ("name: ", name)
+name = 6
+ref_value(name=100, x=200, y=300)
+
+def printargs(*args):
+    print(args)
+printargs(1,'aa')
+
+def func(spam, eggs, toast=0, ham=0):
+    print(spam, eggs, toast, ham)
+func(1,2)
+func(1,ham=1, eggs=0)
+func(spam=1, eggs=0)
+func(toast=1, eggs=2, spam=3)
+func(1,2,3,4)
+
+
+def intersect(*args):
+    res = []
+    for x in args[0]:
+        for other in args[1:]:
+            if x not in other: break
+        else:
+            res.append(x)
+    return res
+
+print(intersect('SPAM', 'SCAM', 'SMDCD'))
